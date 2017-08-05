519d5738e7291cfd0591bf9e67cb5ba8d6538468
nkl05 <abhilashy71@gmail.com>
2017-08-05 11:36:04 +0530

    spychat concept completed.

---
+++ Spychat.py
@@ -0,0 +1,6 @@
+print "let's get started"
+spy_name = raw_input ("what is your name..?:")
+spy_salutation_name = raw_input ("what you want to call yourself like mr or dr or something else")
+spy_name = spy_salutation_name + " " +spy_name
+print "Welcome "+ spy_name + " glad to have you with me"
+
